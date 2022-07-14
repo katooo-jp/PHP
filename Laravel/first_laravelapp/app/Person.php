3c5f849bdbe0a1d832aedefc9e2bb290c42341d4
katooo-jp <katooo.py@gmail.com>
2022-07-14 15:01:18 +0900

    kouka2

--- Laravel/first_laravelapp/app/Person.php
+++ Laravel/first_laravelapp/app/Person.php
@@ -6,21 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model {
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
     public function getData() {
         return $this->id . ':' . $this->name . '(' . $this->age . ')';
     }
 
-    public function scopeNameEqual($query, $str) {
-        return $query->where('name', $str);
-    }
+    // public function scopeNameEqual($query, $str) {
+    //     return $query->where('name', $str);
+    // }
 
-    protected static function boot() {
-        parent::boot();
+    // protected static function boot() {
+    //     parent::boot();
 
-        static::addGlobalScope('age', function (Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
-    }
+    //     static::addGlobalScope('age', function (Builder $builder) {
+    //         $builder->where('age', '>', 20);
+    //     });
+    // }
 }
 
 // model名がPersonなのはlaravelの命名規則でDBは複数形、model名は単数形となっている為
--- Laravel/first_laravelapp/routes/web.php
+++ Laravel/first_laravelapp/routes/web.php
@@ -45,6 +45,20 @@ Route::post('jissyu4_3', 'Jissyu4_3Controller@post');
 
 Route::get('jissyu4_4', 'Jissyu4_4Controller@index');
 Route::post('jissyu4_4', 'Jissyu4_4Controller@post');
+
+Route::get('jissyu6_1', 'Jissyu6_1Controller@index');
+Route::post('jissyu6_1', 'Jissyu6_1Controller@find');
+
+Route::get('jissyu6_3', 'Jissyu6_3Controller@index');
+Route::post('jissyu6_3/find', 'Jissyu6_3Controller@find');
+Route::get('jissyu6_3/show', 'Jissyu6_3Controller@show');
+Route::get('jissyu6_3/add', 'Jissyu6_3Controller@add');
+Route::post('jissyu6_3/create', 'Jissyu6_3Controller@create');
+Route::get('jissyu6_3/edit', 'Jissyu6_3Controller@edit');
+Route::post('jissyu6_3/update', 'Jissyu6_3Controller@update');
+Route::get('jissyu6_3/del', 'Jissyu6_3Controller@del');
+Route::post('jissyu6_3/remove', 'Jissyu6_3Controller@remove');
+
 // ------------------------------------------
 
 
@@ -55,3 +69,18 @@ Route::get('kouka1_1', 'Kouka1_1Controller@index');
 //問題2
 Route::get('kouka1_2', 'Kouka1_2Controller@index');
 Route::post('kouka1_2', 'Kouka1_2Controller@post');
+
+// テスト（laravel効果測定2回）
+//問題1
+Route::get('kouka2_1', 'Kouka2_1Controller@index');
+Route::post('kouka2_1/find', 'Kouka2_1Controller@find');
+//問題2
+Route::get('kouka2_2', 'Kouka2_2Controller@index');
+Route::post('kouka2_2/find', 'Kouka2_2Controller@find');
+Route::get('kouka2_2/show', 'Kouka2_2Controller@show');
+Route::get('kouka2_2/add', 'Kouka2_2Controller@add');
+Route::post('kouka2_2/create', 'Kouka2_2Controller@create');
+Route::get('kouka2_2/edit', 'Kouka2_2Controller@edit');
+Route::post('kouka2_2/update', 'Kouka2_2Controller@update');
+Route::get('kouka2_2/del', 'Kouka2_2Controller@del');
+Route::post('kouka2_2/remove', 'Kouka2_2Controller@remove');
